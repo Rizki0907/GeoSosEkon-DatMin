f691224e4a68fed72fb5e1bd400aa7eff7934335
Rizki Piji Fathoni <rizkipiji0907@gmail.com>
2026-06-01 23:25:32 +0700

    Fix routing bug on Streamlit Cloud by changing target=_top to _self

--- dashboard/app.py
+++ dashboard/app.py
@@ -88,7 +88,7 @@ nav_html = '<div class="nav-wrapper"><div class="nav-logo"><div class="nav-dot">
 for item, icon in NAV_ITEMS.items():
     is_active = page == item
     active_cls = ' class="active"' if is_active else ''
-    nav_html += f'<a href="?page={PAGE_SLUGS[item]}" target="_top"{active_cls}><i class="fa-solid {icon}" style="font-size:0.8rem;"></i> {item}</a>'
+    nav_html += f'<a href="?page={PAGE_SLUGS[item]}" target="_self"{active_cls}><i class="fa-solid {icon}" style="font-size:0.8rem;"></i> {item}</a>'
 nav_html += '</div><div class="nav-team">DatMin Project &bull; 2026</div></div>'
 st.markdown(nav_html, unsafe_allow_html=True)
 
